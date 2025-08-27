7ba8ba4df1cf053e9ec0e66e8ab3afe7d23eab08
kimsw <kimsw021104@gmail.com>
2025-08-27 12:12:08 +0900

    ai chat create

--- src/main/java/com/studycoAchl/hackaton/entity/ChatSession.java
+++ src/main/java/com/studycoAchl/hackaton/entity/ChatSession.java
@@ -23,7 +23,7 @@ public class ChatSession {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "uuid")
+    @Column(name = "UUID")
     private UUID uuid;
 
     @Column(name = "chatTitle", length = 200)
@@ -59,12 +59,12 @@ public class ChatSession {
 
     // JPA 관계 매핑
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_uuid", referencedColumnName = "uuid")
+    @JoinColumn(name = "user_uuid", referencedColumnName = "UUID")
     @JsonBackReference
     private AppUsers user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "subject_uuid", referencedColumnName = "uuid")
+    @JoinColumn(name = "subject_uuid", referencedColumnName = "UUID")
     private Subject subject;
 
     @OneToMany(mappedBy = "chatSession", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
